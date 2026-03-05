992d16a453afe7dc0da1a14d9997f94fa27afae3
Vigneshwar485 <vt3093@srmist.edu.in>
2026-03-05 09:22:11 +0530

    Base application setup

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,19 @@
 
-    public class PalindromeCheckerApp {
+public class PalindromeCheckerApp {
+    public  static void main(String[] args) {
 
-            public  static void main(String[] args) {
+        String input = "madam";
+        String reversed = "";
 
-                String input = "madam";
-                boolean isPalindrome = true;
 
-                for (int i = 0; i < input.length() / 2; i++) {
-                    if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                        isPalindrome = false;
-                        break;
-                    }
-                }
-
-                if (isPalindrome) {
-                    System.out.println(input + " is a Palindrome");
-                } else {
-                    System.out.println(input + " is not a Palindrome");
-                }
-            }
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
-
-
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
+    }
+}
